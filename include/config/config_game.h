cfbea1a8a16e5a82cb1518518f372550950c4a4a
Mew <Staco@live.de>
2025-08-29 02:45:52 +0200

    first config setup
    some base settings

--- include/config/config_game.h
+++ include/config/config_game.h
@@ -53,7 +53,7 @@
 /**
  * Number of possible unique model ID's (keep it higher than 256).
  */
-#define MODEL_ID_COUNT 256
+#define MODEL_ID_COUNT 4096
 
 /**
  * Number of supported areas per level.
